2aee1cfaf014b1a9bb58b00368f5a29c6b63fa2b
Chris Weeden <cweeden@adadapted.com>
2015-04-06 12:20:30 -0400

    DROID-1 Created some empty test classes for rough unit testing of the SDK.

---
+++ sdk/src/androidTest/java/com/adadapted/android/sdk/AdBuilderTest.java
@@ -0,0 +1,19 @@
+package com.adadapted.android.sdk;
+
+
+import org.junit.Test;
+
+/**
+ * Created by chrisweeden on 4/6/15.
+ */
+public class AdBuilderTest {
+    @Test
+    public void testBuildAds_WithCorrectJSON_ShouldReturnAdSet() {
+
+    }
+
+    @Test
+    public void testBuildAd_WithCorrectJSON_ShouldReturnAdObject() {
+
+    }
+}
---
+++ sdk/src/androidTest/java/com/adadapted/android/sdk/EventRequestBuilderTest.java
@@ -0,0 +1,10 @@
+package com.adadapted.android.sdk;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by chrisweeden on 4/6/15.
+ */
+public class EventRequestBuilderTest {
+
+}
