730e837907a9f5e2bcb9b15daeb6435646494c9f
Chris Weeden <cweeden@adadapted.com>
2015-04-28 13:17:28 -0400

    DROID-3 Implemented some class details.

--- sdk/src/main/java/com/adadapted/android/sdk/core/ad/HtmlAdType.java
+++ sdk/src/main/java/com/adadapted/android/sdk/core/ad/HtmlAdType.java
@@ -4,7 +4,17 @@ package com.adadapted.android.sdk.core.ad;
  * Created by chrisweeden on 4/15/15.
  */
 public class HtmlAdType extends AdType {
+    private String adUrl;
+
     public HtmlAdType() {
         this.setAdType(AdTypes.HTML);
+    }
+
+    public String getAdUrl() {
+        return adUrl;
+    }
+
+    public void setAdUrl(String adUrl) {
+        this.adUrl = adUrl;
     }
 }
