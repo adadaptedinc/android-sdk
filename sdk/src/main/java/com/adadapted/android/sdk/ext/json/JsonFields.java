dfba314bcfab6947dece50289bffeba10528a9cc
Chris Weeden <cweeden@adadapted.com>
2015-05-05 14:35:53 -0400

    DROID-3 Created a class with a number of field constants.

---
+++ sdk/src/main/java/com/adadapted/android/sdk/ext/json/JsonFields.java
@@ -0,0 +1,23 @@
+package com.adadapted.android.sdk.ext.json;
+
+/**
+ * Created by chrisweeden on 4/28/15.
+ */
+public class JsonFields {
+    public static final String APPID = "app_id";
+    public static final String UDID = "udid";
+    public static final String BUNDLEID = "bundle_id";
+    public static final String ZONES = "zones";
+    public static final String DEVICE = "device";
+    public static final String OS = "os";
+    public static final String OSV = "osv";
+    public static final String LOCALE = "locale";
+    public static final String TIMEZONE = "timezone";
+    public static final String DATETIME = "datetime";
+    public static final String DH = "dh";
+    public static final String DW = "dw";
+    public static final String ALLOWRETARGETING = "allow_retargeting";
+    public static final String SDKVERSION = "sdk_version";
+
+    public static final String SESSIONID = "session_id";
+}
