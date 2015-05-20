5c5f67dcaa9511eae8dbb5e56a0fc4f319bb87c7
Chris Weeden <cweeden@adadapted.com>
2015-05-20 15:26:32 -0400

    DROID-3 Updated the formatting of screen resolutions.

---
+++ sdk/src/main/java/com/adadapted/android/sdk/core/device/ScreenDensity.java
@@ -0,0 +1,15 @@
+package com.adadapted.android.sdk.core.device;
+
+/**
+ * Created by chrisweeden on 5/20/15.
+ */
+public enum ScreenDensity {
+    HPDI,
+    MDPI,
+    LDPI,
+    TV,
+    XHDPI,
+    XXHDPI,
+    XXXHDPI,
+    UNKNOWN
+}
