81813ab7546b23be2a056135b0144611ba5dbe5d
Chris Weeden <cweeden@adadapted.com>
2015-06-04 11:26:59 -0400

    Created an item to be returned when getItem is called for a position that contains an Ad.

---
+++ sdk/src/main/java/com/adadapted/android/sdk/ui/adapter/AAFeedItem.java
@@ -0,0 +1,11 @@
+package com.adadapted.android.sdk.ui.adapter;
+
+/**
+ * Created by chrisweeden on 6/3/15.
+ */
+public class AAFeedItem {
+    @Override
+    public String toString() {
+        return "AAFeedItem{}";
+    }
+}
