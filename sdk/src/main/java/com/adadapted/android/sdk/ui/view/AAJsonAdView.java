0997dd773e9bab370b1981d44714f94143fd7df8
Chris Weeden <cweeden@adadapted.com>
2015-06-23 14:24:40 -0400

    Made some steps forward in using this view. Presently it still really isn't useful.

--- sdk/src/main/java/com/adadapted/android/sdk/ui/view/AAJsonAdView.java
+++ sdk/src/main/java/com/adadapted/android/sdk/ui/view/AAJsonAdView.java
@@ -1,6 +1,9 @@
 package com.adadapted.android.sdk.ui.view;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
 import android.view.View;
 
 import com.adadapted.android.sdk.core.ad.model.Ad;
@@ -17,13 +20,26 @@ public class AAJsonAdView extends View implements AdViewListenable {
     private static final String TAG = AAHtmlAdView.class.getName();
 
     private Set<AdViewListener> listeners;
+    private View view;
 
     public AAJsonAdView(Context context, int resourceId) {
         super(context);
 
         listeners = new HashSet<>();
+        view = View.inflate(context, resourceId, null);
+    }
 
-        View view = View.inflate(context, resourceId, null);
+    public AAJsonAdView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AAJsonAdView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public AAJsonAdView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
     }
 
     public void buildView(Ad ad) {
@@ -32,11 +48,11 @@ public class AAJsonAdView extends View implements AdViewListenable {
 
     @Override
     public void addListener(AdViewListener listener) {
-
+        listeners.add(listener);
     }
 
     @Override
     public void removeListener(AdViewListener listener) {
-
+        listeners.remove(listener);
     }
 }
