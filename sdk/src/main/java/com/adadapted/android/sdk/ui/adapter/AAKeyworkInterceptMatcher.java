eb85e0a4ead3414874654f2370fceb824b06eddc
Chris Weeden <cweeden@adadapted.com>
2015-06-29 12:25:59 -0400

    Implemented a matcher that uses the autofill Map from the Keyword Intercept to match purchased keywords and return them.

---
+++ sdk/src/main/java/com/adadapted/android/sdk/ui/adapter/AAKeyworkInterceptMatcher.java
@@ -0,0 +1,74 @@
+package com.adadapted.android.sdk.ui.adapter;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.adadapted.android.sdk.AdAdapted;
+import com.adadapted.android.sdk.core.keywordintercept.KeywordInterceptManager;
+import com.adadapted.android.sdk.core.keywordintercept.model.KeywordIntercept;
+import com.adadapted.android.sdk.core.session.SessionManager;
+import com.adadapted.android.sdk.core.session.model.Session;
+import com.adadapted.android.sdk.ext.factory.KeywordInterceptManagerFactory;
+import com.adadapted.android.sdk.ext.factory.SessionManagerFactory;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by chrisweeden on 6/25/15.
+ */
+public class AAKeyworkInterceptMatcher implements SessionManager.Listener, KeywordInterceptManager.Listener {
+    private static final String TAG = AAKeyworkInterceptMatcher.class.getName();
+
+    private final KeywordInterceptManager manager;
+    
+    private KeywordIntercept keywordIntercept;
+    private boolean loaded = false;
+    private Session session;
+
+    public AAKeyworkInterceptMatcher(Context context) {
+        manager = KeywordInterceptManagerFactory.getInstance(context).createKeywordInterceptManager();
+        manager.addListener(this);
+
+        SessionManagerFactory.getInstance(context).createSessionManager().addListener(this);
+    }
+
+    public Set<String> match(CharSequence constraint) {
+        Set<String> suggestions = new HashSet<>();
+
+        if(constraint != null && isLoaded()) {
+            for (String item : keywordIntercept.getAutofill().keySet()) {
+                if (item.toLowerCase().contains(constraint.toString().toLowerCase())) {
+                    suggestions.add(keywordIntercept.getAutofill().get(item));
+                    manager.trackPresented(session, item, constraint.toString());
+                }
+            }
+        }
+
+        return suggestions;
+    }
+
+    private boolean isLoaded() {
+        return loaded;
+    }
+
+    @Override
+    public void onInitSuccess(KeywordIntercept keywordIntercept) {
+        Log.d(TAG, "Keyword Matcher Initialized.");
+        this.keywordIntercept = keywordIntercept;
+        this.loaded = true;
+    }
+
+    @Override
+    public void onSessionInitialized(Session session) {
+        Log.d(TAG, "Session Initialized.");
+        this.session = session;
+        manager.init(session, AdAdapted.getInstance().getDeviceInfo());
+    }
+
+    @Override
+    public void onSessionInitFailed() {}
+
+    @Override
+    public void onSessionNotReinitialized() {}
+}
