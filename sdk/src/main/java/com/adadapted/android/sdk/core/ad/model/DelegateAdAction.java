a08c4d4f6cfe30b7bc25e82f4e854b940dbb12b1
Chris Weeden <cweeden@adadapted.com>
2015-06-29 12:04:12 -0400

    Added support for additional Action Types.

---
+++ sdk/src/main/java/com/adadapted/android/sdk/core/ad/model/DelegateAdAction.java
@@ -0,0 +1,10 @@
+package com.adadapted.android.sdk.core.ad.model;
+
+/**
+ * Created by chrisweeden on 6/25/15.
+ */
+public class DelegateAdAction extends AdAction {
+    public DelegateAdAction() {
+        super(DELEGATE);
+    }
+}
