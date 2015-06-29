06243303378b3bc0323d6118697c1b7ed907c503
Chris Weeden <cweeden@adadapted.com>
2015-06-29 12:08:13 -0400

    Updated type to have actual support for JSON.

--- sdk/src/main/java/com/adadapted/android/sdk/core/ad/model/JsonAdType.java
+++ sdk/src/main/java/com/adadapted/android/sdk/core/ad/model/JsonAdType.java
@@ -1,10 +1,22 @@
 package com.adadapted.android.sdk.core.ad.model;
 
+import org.json.JSONObject;
+
 /**
  * Created by chrisweeden on 4/15/15.
  */
 public class JsonAdType extends AdType {
+    private JSONObject json;
+
     public JsonAdType() {
         setType(AdTypes.JSON);
+    }
+
+    public JSONObject getJson() {
+        return json;
+    }
+
+    public void json(JSONObject json) {
+
     }
 }
