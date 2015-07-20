99d1be11bf02a45194648fccb93ca162c9c3996e
Chris Weeden <cweeden@adadapted.com>
2015-07-20 16:11:56 -0400

    Implemented an object to be passed to the hosting App for use with the Keyword Intercept implementation.

--- sdk/src/main/java/com/adadapted/android/sdk/ui/model/SuggestionPayload.java
+++ sdk/src/main/java/com/adadapted/android/sdk/ui/model/SuggestionPayload.java
@@ -1,7 +1,30 @@
 package com.adadapted.android.sdk.ui.model;
 
+import com.adadapted.android.sdk.ui.adapter.AaSuggestionTracker;
+
+import java.util.Set;
+
 /**
  * Created by chrisweeden on 7/17/15.
  */
 public class SuggestionPayload {
+    private final AaSuggestionTracker suggestionTracker;
+    private Set<String> suggestions;
+
+    public SuggestionPayload(AaSuggestionTracker suggestionTracker, Set<String> suggestions) {
+        this.suggestionTracker = suggestionTracker;
+        this.suggestions = suggestions;
+    }
+
+    public Set<String> getSuggestions() {
+        return suggestions;
+    }
+
+    public void presented(String term) {
+        suggestionTracker.suggestionPresented(term);
+    }
+
+    public void selected(String term) {
+        suggestionTracker.suggestionSelected(term);
+    }
 }
