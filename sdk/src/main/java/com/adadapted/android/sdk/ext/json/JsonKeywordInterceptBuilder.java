dcba731e9eee4c04060ee619b87ba9e9ad981cf1
Chris Weeden <cweeden@adadapted.com>
2015-07-20 15:52:14 -0400

    Now parsing the JSON response from the server rather than just building a static response.

--- sdk/src/main/java/com/adadapted/android/sdk/ext/json/JsonKeywordInterceptBuilder.java
+++ sdk/src/main/java/com/adadapted/android/sdk/ext/json/JsonKeywordInterceptBuilder.java
@@ -1,33 +1,83 @@
 package com.adadapted.android.sdk.ext.json;
 
-import com.adadapted.android.sdk.core.ad.model.Ad;
+import android.util.Log;
+
 import com.adadapted.android.sdk.core.keywordintercept.KeywordInterceptBuilder;
+import com.adadapted.android.sdk.core.keywordintercept.model.AutoFill;
 import com.adadapted.android.sdk.core.keywordintercept.model.KeywordIntercept;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
  * Created by chrisweeden on 6/25/15.
  */
 public class JsonKeywordInterceptBuilder implements KeywordInterceptBuilder {
+    private static final String TAG = JsonKeywordInterceptBuilder.class.getName();
+
     public KeywordIntercept build(JSONObject json) {
-        return new KeywordIntercept("", 0L, parseAutofill(json), parseTrigger(json));
+
+        String searchId = "";
+        long refreshTime = 0L;
+
+        try {
+            if (json.has("search_id")) {
+                searchId = json.getString("search_id");
+            }
+
+            if (json.has("refresh_time")) {
+                refreshTime = Long.parseLong(json.getString("refresh_time"));
+            }
+        }
+        catch(JSONException ex) {
+            Log.w(TAG, "Problem parsing JSON", ex);
+        }
+
+        Map<String, AutoFill> interceptMap = parseAutofill(json);
+
+        return new KeywordIntercept(searchId, refreshTime, interceptMap);
     }
 
-    private Map<String, String> parseAutofill(JSONObject json) {
-        Map<String, String> interceptMap = new HashMap<>();
-        interceptMap.put("Yogurt", "Chobani® Greek Yogurt");
-        interceptMap.put("Sour Cream", "Chobani® Greek Yogurt");
-        interceptMap.put("Mayonaise", "Chobani® Greek Yogurt");
-        interceptMap.put("Chobani® Greek Yogurt", "Chobani® Greek Yogurt");
+    private Map<String, AutoFill> parseAutofill(JSONObject json) {
+        Map<String, AutoFill> interceptMap = new HashMap<>();
+
+        try {
+            if(json.has("autofill")) {
+                JSONObject autofillJson = json.getJSONObject("autofill");
+                for(Iterator<String> z = autofillJson.keys(); z.hasNext();) {
+                    String term = z.next();
+                    JSONObject jsonTerm = autofillJson.getJSONObject(term);
+
+                    String replacement = "";
+                    String icon = "";
+                    String tagline = "";
+
+                    if (jsonTerm.has("replacement")) {
+                        replacement = jsonTerm.getString("replacement");
+                    }
+
+                    if (jsonTerm.has("icon")) {
+                        icon = jsonTerm.getString("icon");
+                    }
+
+                    if (jsonTerm.has("tagline")) {
+                        tagline = jsonTerm.getString("tagline");
+                    }
+
+                    AutoFill autofill = new AutoFill(replacement, icon, tagline);
+
+                    interceptMap.put(term, autofill);
+                }
+            }
+        }
+        catch(JSONException ex) {
+            Log.w(TAG, "Problem parsing JSON", ex);
+        }
 
         return interceptMap;
-    }
-
-    private HashMap<String, Ad> parseTrigger(JSONObject json) {
-        return new HashMap<>();
     }
 }
