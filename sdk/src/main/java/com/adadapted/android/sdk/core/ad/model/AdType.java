52ea6b99526e2b04ffa0d572fa8230ef6db2e060
Chris Weeden <cweeden@adadapted.com>
2015-07-20 13:46:19 -0400

    Made serialization id private.

--- sdk/src/main/java/com/adadapted/android/sdk/core/ad/model/AdType.java
+++ sdk/src/main/java/com/adadapted/android/sdk/core/ad/model/AdType.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
  * Created by chrisweeden on 4/15/15.
  */
 public abstract class AdType implements Serializable {
-    static final long serialVersionUID = 42L;
+    private static final long serialVersionUID = 42L;
 
     private AdTypes adType = AdTypes.NULL;
 
