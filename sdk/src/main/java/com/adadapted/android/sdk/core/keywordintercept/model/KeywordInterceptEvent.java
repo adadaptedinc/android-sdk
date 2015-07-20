c408983bb5c7d90d9ee22df07597b80a565f9c77
Chris Weeden <cweeden@adadapted.com>
2015-07-20 13:57:47 -0400

    Added Search Id field.

--- sdk/src/main/java/com/adadapted/android/sdk/core/keywordintercept/model/KeywordInterceptEvent.java
+++ sdk/src/main/java/com/adadapted/android/sdk/core/keywordintercept/model/KeywordInterceptEvent.java
@@ -6,23 +6,26 @@ import java.util.Date;
  * Created by chrisweeden on 6/29/15.
  */
 public class KeywordInterceptEvent {
+    public static final String MATCHED  = "matched";
     public static final String PRESENTED  = "presented";
     public static final String SELECTED  = "selected";
 
     private final String appId;
     private final String sessionId;
     private final String udid;
+    private final String searchId;
     private final Date datetime;
     private final String event;
     private final String userInput;
     private final String term;
     private final String sdkVersion;
 
-    public KeywordInterceptEvent(String appId, String sessionId, String udid, String event,
-                                 String userInput, String term, String sdkVersion) {
+    public KeywordInterceptEvent(String appId, String sessionId, String udid, String searchId,
+                                 String event, String userInput, String term, String sdkVersion) {
         this.appId = appId;
         this.sessionId = sessionId;
         this.udid = udid;
+        this.searchId = searchId;
         this.datetime = new Date();
         this.event = event;
         this.userInput = userInput;
@@ -40,6 +43,10 @@ public class KeywordInterceptEvent {
 
     public String getUdid() {
         return udid;
+    }
+
+    public String getSearchId() {
+        return searchId;
     }
 
     public Date getDatetime() {
@@ -63,9 +70,10 @@ public class KeywordInterceptEvent {
     }
 
     public boolean supercedes(KeywordInterceptEvent e) {
-        return this.sessionId.equals(e.getSessionId()) &&
-                this.term.equals(e.getTerm()) &&
-                this.userInput.contains(e.getUserInput());
+        return sessionId.equals(e.getSessionId()) &&
+                event.equals(e.getEvent()) &&
+                term.equals(e.getTerm()) &&
+                userInput.contains(e.getUserInput());
     }
 
     @Override
