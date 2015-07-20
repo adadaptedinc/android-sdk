fdd8f5fcf8d673031361cdd409d50cd8711d96e6
Chris Weeden <cweeden@adadapted.com>
2015-07-20 16:13:20 -0400

    Implemented a helper for handling the tracking of active Keyword Interaction suggestions.

--- sdk/src/main/java/com/adadapted/android/sdk/ui/adapter/AaSuggestionTracker.java
+++ sdk/src/main/java/com/adadapted/android/sdk/ui/adapter/AaSuggestionTracker.java
@@ -1,7 +1,52 @@
 package com.adadapted.android.sdk.ui.adapter;
 
+import com.adadapted.android.sdk.core.keywordintercept.KeywordInterceptManager;
+import com.adadapted.android.sdk.core.session.model.Session;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by chrisweeden on 7/16/15.
  */
 public class AaSuggestionTracker {
+    private static final String TAG = AaSuggestionTracker.class.getName();
+
+    private final KeywordInterceptManager manager;
+
+    private Map<String, String> items = new HashMap<>();
+    private Map<String, String> replacements = new HashMap<>();
+
+    private Session session;
+
+    AaSuggestionTracker(KeywordInterceptManager manager) {
+        this.manager = manager;
+    }
+
+    void suggestionMatched(Session session, String term, String replacement, String userInput) {
+        this.session = session;
+
+        term = term.toLowerCase();
+        userInput = userInput.toLowerCase();
+        replacement = replacement.toLowerCase();
+
+        items.put(term, userInput);
+        replacements.put(replacement, term);
+
+        manager.trackMatched(session, term, userInput);
+    }
+
+    public void suggestionPresented(String term) {
+        term = term.toLowerCase();
+        if(items.containsKey(term)) {
+            manager.trackPresented(session, term, items.get(term));
+        }
+    }
+
+    public void suggestionSelected(String replacement) {
+        if(items.containsKey(replacement)) {
+            String term = replacements.get(replacement);
+            manager.trackSelected(session, term, replacement);
+        }
+    }
 }
