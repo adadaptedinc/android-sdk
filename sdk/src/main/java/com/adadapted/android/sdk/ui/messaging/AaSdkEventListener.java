67a33cb9a7d23c984a4ac7c48a15045ded34b40e
Chris Weeden <cweeden@adadapted.com>
2016-02-23 15:53:30 -0500

    Moved the listener for whether a Session has Ads from the Event publisher to a Session Listener handled in a different fashion.

--- sdk/src/main/java/com/adadapted/android/sdk/ui/messaging/AaSdkEventListener.java
+++ sdk/src/main/java/com/adadapted/android/sdk/ui/messaging/AaSdkEventListener.java
@@ -4,6 +4,5 @@ package com.adadapted.android.sdk.ui.messaging;
  * Created by chrisweeden on 8/18/15.
  */
 public interface AaSdkEventListener {
-    void onHasAdsToServe(boolean hasAds);
     void onNextAdEvent(String zoneId, String eventType);
 }
