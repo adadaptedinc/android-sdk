97a23bddc5f4aea68559054c53e80f69915216e6
Chris Weeden <cweeden@adadapted.com>
2016-04-26 09:57:47 -0400

    Tore out the existing project and replaced it with a fresh project that contains an app and the sdk library module.

--- sdk/src/main/java/com/adadapted/android/sdk/core/ad/model/NullAdType.java
+++
@@ -1,7 +0,0 @@
-package com.adadapted.android.sdk.core.ad.model;
-
-/**
- * Created by chrisweeden on 4/15/15.
- */
-public class NullAdType extends AdType {
-}
--- sdk/src/main/java/com/adadapted/android/sdk/ui/messaging/AaSdkEventListener.java
+++
@@ -1,8 +0,0 @@
-package com.adadapted.android.sdk.ui.messaging;
-
-/**
- * Created by chrisweeden on 8/18/15.
- */
-public interface AaSdkEventListener {
-    void onNextAdEvent(String zoneId, String eventType);
-}
