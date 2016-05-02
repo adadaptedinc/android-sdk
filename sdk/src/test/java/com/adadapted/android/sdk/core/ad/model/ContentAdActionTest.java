de020f23acf84eb1563cb85b903b3f63f757659b
Chris Weeden <cweeden@adadapted.com>
2016-05-02 14:24:01 -0400

    Regenerated Core test classes.

---
+++ sdk/src/test/java/com/adadapted/android/sdk/core/ad/model/ContentAdActionTest.java
@@ -0,0 +1,10 @@
+package com.adadapted.android.sdk.core.ad.model;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by chrisweeden on 5/2/16.
+ */
+public class ContentAdActionTest {
+
+}
---
+++ sdk/src/test/java/com/adadapted/android/sdk/core/keywordintercept/model/KeywordInterceptTest.java
@@ -0,0 +1,10 @@
+package com.adadapted.android.sdk.core.keywordintercept.model;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by chrisweeden on 5/2/16.
+ */
+public class KeywordInterceptTest {
+
+}
