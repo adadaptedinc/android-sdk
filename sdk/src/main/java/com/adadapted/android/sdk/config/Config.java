8216216bd9372947bb3dcf268cbe857cde0858c5
Chris Weeden <cweeden@adadapted.com>
2016-09-16 09:15:43 -0400

    Added url for app event tracking. Rolled the SDK version to 0.10.0 to represent the introduction of App Events

--- sdk/src/main/java/com/adadapted/android/sdk/config/Config.java
+++ sdk/src/main/java/com/adadapted/android/sdk/config/Config.java
@@ -4,7 +4,7 @@ package com.adadapted.android.sdk.config;
  * Created by chrisweeden on 7/27/15.
  */
 public class Config {
-    public static final String SDK_VERSION = "0.9.10";
+    public static final String SDK_VERSION = "0.10.0";
     public static final long DEFAULT_EVENT_POLLING = 20000L;
 
     public static class Prod {
@@ -14,6 +14,8 @@ public class Config {
         public static final String URL_KI_INIT = "https://ads.adadapted.com/v/0.9.3/android/ki/init";
         public static final String URL_KI_TRACK = "https://ads.adadapted.com/v/0.9.3/android/ki/track";
         public static final String URL_CONTENT_TRACK = "https://ads.adadapted.com/v/0.9.3/content/track";
+
+        public static final String URL_APP_EVENT_TRACK = "";
     }
 
     public static class Sand {
@@ -23,5 +25,7 @@ public class Config {
         public static final String URL_KI_INIT = "https://sandbox.adadapted.com/v/0.9.3/android/ki/init";
         public static final String URL_KI_TRACK = "https://sandbox.adadapted.com/v/0.9.3/android/ki/track";
         public static final String URL_CONTENT_TRACK = "https://sandbox.adadapted.com/v/0.9.3/content/track";
+
+        public static final String URL_APP_EVENT_TRACK = "http://eventcollector.t.adadapted.com/v/1/android/events";
     }
 }
