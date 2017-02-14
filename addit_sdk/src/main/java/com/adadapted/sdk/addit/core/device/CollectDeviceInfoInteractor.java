9231dd790f62ced37ba543a1f464708460858840
Chris Weeden <cweeden@adadapted.com>
2017-02-14 10:46:41 -0500

    Updated logic to avoid Null Pointer Exceptions that were generated in some cases.

--- addit_sdk/src/main/java/com/adadapted/sdk/addit/core/device/CollectDeviceInfoInteractor.java
+++ addit_sdk/src/main/java/com/adadapted/sdk/addit/core/device/CollectDeviceInfoInteractor.java
@@ -1,6 +1,8 @@
 package com.adadapted.sdk.addit.core.device;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.provider.Settings;
@@ -22,7 +24,6 @@ import static com.google.ads.AdRequest.LOGTAG;
 /**
  * Created by chrisweeden on 9/26/16.
  */
-
 public class CollectDeviceInfoInteractor implements Interactor {
     private final CollectDeviceInfoCommand command;
     private final Callback callback;
@@ -40,14 +41,22 @@ public class CollectDeviceInfoInteractor implements Interactor {
 
         deviceInfo.setAppId(command.getAppId());
         deviceInfo.setProd(command.isProd());
-        deviceInfo.setScale(context.getResources().getDisplayMetrics().density);
 
-        deviceInfo.setUdid(captureAdvertisingId(context));
+        final AdvertisingIdClient.Info info = getAdvertisingIdClientInfo(context);
+        if (info != null) {
+            deviceInfo.setUdid(info.getId());
+            deviceInfo.setAllowRetargeting(info.isLimitAdTrackingEnabled());
+        }
+        else {
+            deviceInfo.setUdid(captureAndroidId(context));
+            deviceInfo.setAllowRetargeting(false);
+        }
 
         deviceInfo.setBundleId(context.getPackageName());
 
         try {
-            final String version = context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionName;
+            final PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            final String version = packageInfo != null ? packageInfo.versionName : DeviceInfo.UNKNOWN_VALUE;
             deviceInfo.setBundleVersion(version);
         }
         catch(PackageManager.NameNotFoundException ex) {
@@ -63,45 +72,35 @@ public class CollectDeviceInfoInteractor implements Interactor {
         deviceInfo.setLocale(Locale.getDefault().toString());
 
         final TelephonyManager manager = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
-        final String carrier = manager.getNetworkOperatorName().length() > 0 ? manager.getNetworkOperatorName() : "None";
+        final String carrier = (manager != null && manager.getNetworkOperatorName().length() > 0) ? manager.getNetworkOperatorName() : "None";
         deviceInfo.setCarrier(carrier);
 
         final DisplayMetrics metrics = context.getResources().getDisplayMetrics();
-        deviceInfo.setDh(metrics.heightPixels);
-        deviceInfo.setDw(metrics.widthPixels);
-
-        deviceInfo.setDensity(determineScreenDensity(context));
-
-        deviceInfo.setAllowRetargeting(captureRetargetingEnabled(context));
+        if (metrics != null) {
+            deviceInfo.setScale(metrics.density);
+            deviceInfo.setDh(metrics.heightPixels);
+            deviceInfo.setDw(metrics.widthPixels);
+            deviceInfo.setDensity(determineScreenDensity(metrics));
+        }
 
         deviceInfo.setSdkVersion(command.getSdkVersion());
 
         callback.onDeviceInfoCollected(deviceInfo);
     }
 
-    private String captureAdvertisingId(final Context context) {
+    private AdvertisingIdClient.Info getAdvertisingIdClientInfo(final Context context) {
         try {
-            return AdvertisingIdClient.getAdvertisingIdInfo(context).getId();
+            return AdvertisingIdClient.getAdvertisingIdInfo(context);
         }
         catch (GooglePlayServicesNotAvailableException | GooglePlayServicesRepairableException | IOException ex) {
             Log.w(LOGTAG, "Problem retrieving Google Play Advertiser Info");
         }
 
-        return captureAndroidId(context);
-    }
-
-    private boolean captureRetargetingEnabled(Context context) {
-        try {
-            return !AdvertisingIdClient.getAdvertisingIdInfo(context).isLimitAdTrackingEnabled();
-        }
-        catch (GooglePlayServicesNotAvailableException | GooglePlayServicesRepairableException | IOException ex) {
-            Log.w(LOGTAG, "Problem retrieving Google Play Advertiser Info");
-        }
-
-        return true;
+        return null;
     }
 
     private String captureAndroidId(final Context context) {
+        @SuppressLint("HardwareIds")
         final String androidId = Settings.Secure.getString(
                 context.getContentResolver(),
                 Settings.Secure.ANDROID_ID);
@@ -109,8 +108,8 @@ public class CollectDeviceInfoInteractor implements Interactor {
         return androidId == null ? "" : androidId;
     }
 
-    private ScreenDensity determineScreenDensity(final Context context) {
-        int density = context.getResources().getDisplayMetrics().densityDpi;
+    private ScreenDensity determineScreenDensity(final DisplayMetrics metrics) {
+        int density = metrics.densityDpi;
         switch (density)
         {
             case DisplayMetrics.DENSITY_MEDIUM:
