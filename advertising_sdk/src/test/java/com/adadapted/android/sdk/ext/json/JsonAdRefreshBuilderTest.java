3dafda04d2faa0c2a0e602b20092f70e6e6f9854
Chris Weeden <cweeden@adadapted.com>
2017-05-10 09:12:11 -0400

    Added Unit Tests for main classes.

---
+++ advertising_sdk/src/test/java/com/adadapted/android/sdk/ext/json/JsonAdRefreshBuilderTest.java
@@ -0,0 +1,22 @@
+package com.adadapted.android.sdk.ext.json;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by chrisweeden on 5/10/17.
+ */
+public class JsonAdRefreshBuilderTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}
