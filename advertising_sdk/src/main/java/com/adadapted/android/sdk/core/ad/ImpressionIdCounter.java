5a738687a1d2748554313b6d26bb753098b4db8a
Chris Weeden <cweeden@adadapted.com>
2018-04-09 12:13:56 -0400

    Added an impression counter to track how many times an instance of an Ad is shown

---
+++ advertising_sdk/src/main/java/com/adadapted/android/sdk/core/ad/ImpressionIdCounter.java
@@ -0,0 +1,64 @@
+package com.adadapted.android.sdk.core.ad;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ImpressionIdCounter {
+    private static ImpressionIdCounter instance;
+
+    public static ImpressionIdCounter getsInstance() {
+        if(instance == null) {
+            instance = new ImpressionIdCounter();
+        }
+
+        return instance;
+    }
+
+    private static final int INITIAL_VAL = 1;
+
+    private final Map<String, Integer> idCounts;
+    private final Lock counterLock = new ReentrantLock();
+
+    private ImpressionIdCounter() {
+        idCounts = new HashMap<>();
+    }
+
+    private int initCountFor(final String impressionId) {
+        idCounts.put(impressionId, INITIAL_VAL);
+        return INITIAL_VAL;
+    }
+
+    public synchronized int getIncrementedCountFor(final String impressionId) {
+        counterLock.lock();
+        try {
+            if (idCounts.containsKey(impressionId)) {
+                int val = idCounts.get(impressionId);
+                val++;
+                idCounts.put(impressionId, val);
+
+                return val;
+            } else {
+                return initCountFor(impressionId);
+            }
+        }
+        finally {
+            counterLock.unlock();
+        }
+    }
+
+    public synchronized int getCurrentCountFor(final String impressionId) {
+        counterLock.lock();
+        try {
+            if (idCounts.containsKey(impressionId)) {
+                return idCounts.get(impressionId);
+            } else {
+                return initCountFor(impressionId);
+            }
+        }
+        finally {
+            counterLock.unlock();
+        }
+    }
+}
