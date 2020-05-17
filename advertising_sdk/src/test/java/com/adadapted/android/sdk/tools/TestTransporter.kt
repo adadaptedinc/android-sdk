6928079353080bc3b4a8176354344e25624f8b96
bclifton <bclifton@adadapted.com>
2020-05-17 11:55:36 -0400

    Updating event client and refactoring thread caller

---
+++ advertising_sdk/src/test/java/com/adadapted/android/sdk/tools/TestTransporter.kt
@@ -0,0 +1,18 @@
+package com.adadapted.android.sdk.tools
+
+import com.adadapted.android.sdk.core.concurrency.TransporterCoroutineScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.TestCoroutineScope
+import kotlin.coroutines.CoroutineContext
+
+class TestTransporter(override val coroutineContext: CoroutineContext): TransporterCoroutineScope {
+    private val scope = TestCoroutineScope(coroutineContext)
+
+    override fun dispatchToBackground(backgroundFunc: suspend CoroutineScope.() -> Unit): Job {
+        return scope.launch {
+            backgroundFunc.invoke(this)
+        }
+    }
+}
