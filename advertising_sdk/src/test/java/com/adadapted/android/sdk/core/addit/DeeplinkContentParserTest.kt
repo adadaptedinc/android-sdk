3cee90cccb09d1839352fc865832d8809404377b
Brett Clifton <bclifton@adadapted.com>
2023-11-13 12:15:47 -0500

    feat: updating robolectric and enabling tests that required it

--- advertising_sdk/src/test/java/com/adadapted/android/sdk/core/addit/DeeplinkContentParserTest.kt
+++ advertising_sdk/src/test/java/com/adadapted/android/sdk/core/addit/DeeplinkContentParserTest.kt
@@ -1,7 +1,6 @@
 package com.adadapted.android.sdk.core.addit
 
 import android.net.Uri
-import androidx.core.net.toUri
 import com.adadapted.android.sdk.constants.EventStrings
 import com.adadapted.android.sdk.core.atl.AddToListContent
 import com.adadapted.android.sdk.core.atl.AdditContent
@@ -24,73 +23,73 @@ import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import java.lang.Exception
 
-//@OptIn(ExperimentalCoroutinesApi::class)
-//@RunWith(RobolectricTestRunner::class)
-//class DeeplinkContentParserTest {
-//    private var testTransporter = UnconfinedTestDispatcher()
-//    private val testTransporterScope: TransporterCoroutineScope = TestTransporter(testTransporter)
-//
-//    @Before
-//    fun setup() {
-//        DeviceInfoClient.createInstance("", false, HashMap(), "", TestDeviceInfoExtractor(), testTransporterScope)
-//        SessionClient.createInstance(mock(), mock())
-//        EventClient.createInstance(TestEventAdapter, testTransporterScope)
-//        PayloadClient.createInstance(mock(), EventClient, mock())
-//    }
-//
-//    @Test
-//    fun parse() {
-//        val uri = Uri.parse(TEST_URL_STRING_ITEMS)
-//        val parser = DeeplinkContentParser()
-//        val content = parser.parse(uri)
-//        Assert.assertEquals("7498E63F-BF44-466B-A391-8B6721C32FF7", content.payloadId)
-//        Assert.assertEquals("Sample Product", content.message)
-//        Assert.assertEquals("", content.image)
-//        Assert.assertEquals(AdditContent.AdditSources.DEEPLINK, content.additSource)
-//        Assert.assertEquals(AddToListContent.Sources.OUT_OF_APP, content.getSource())
-//        Assert.assertEquals(1, content.getItems().size.toLong())
-//    }
-//
-//    @Test
-//    fun fail() {
-//        val parser = DeeplinkContentParser()
-//
-//        try {
-//            parser.parse(null)
-//            Assert.assertEquals(EventStrings.ADDIT_NO_DEEPLINK_RECEIVED, TestEventAdapter.testSdkErrors.first().code)
-//            Assert.fail()
-//        } catch (ex: Exception) {
-//            //success
-//        }
-//    }
-//
-//    @Test
-//    fun failWithParseFailure() {
-//        try {
-//            val uri = Uri.parse(TEST_FAIL_URL_STRING)
-//            val parser = DeeplinkContentParser()
-//            parser.parse(uri)
-//            Assert.assertEquals(EventStrings.ADDIT_PAYLOAD_PARSE_FAILED, TestEventAdapter.testSdkErrors.first().code)
-//        } catch (ex: Exception) {
-//            //success
-//        }
-//    }
-//
-//    @Test
-//    fun failWithPayloadFailure() {
-//        try {
-//            val uri = Uri.parse(TEST_UNKNOWN_URL_STRING)
-//            val parser = DeeplinkContentParser()
-//            parser.parse(uri)
-//            Assert.assertEquals(EventStrings.ADDIT_PAYLOAD_PARSE_FAILED, TestEventAdapter.testSdkErrors.first().code)
-//        } catch (ex: Exception) {
-//            //success
-//        }
-//    }
-//
-//    companion object {
-//        private const val TEST_URL_STRING_ITEMS = "droidrecipe://adadapted.com/addit_add_list_items?data=eyJwYXlsb2FkX2lkIjoiNzQ5OEU2M0YtQkY0NC00NjZCLUEzOTEtOEI2NzIxQzMyRkY3IiwicGF5bG9hZF9tZXNzYWdlIjoiU2FtcGxlIFByb2R1Y3QiLCJwYXlsb2FkX2ltYWdlIjoiIiwiY2FtcGFpZ25faWQiOiIyNTQiLCJhcHBfaWQiOiJkcm9pZHJlY2lwZSIsImV4cGlyZV9zZWNvbmRzIjo2MDQ4MDAsImRldGFpbGVkX2xpc3RfaXRlbXMiOlt7InRyYWNraW5nX2lkIjoiRDA2OTk4OTItQkY5RS00RTM1LUI5MkQtQzVEN0YyRDZFOUFEIiwicHJvZHVjdF90aXRsZSI6IlNhbXBsZSBQcm9kdWN0IiwicHJvZHVjdF9icmFuZCI6IkJyYW5kIiwicHJvZHVjdF9jYXRlZ29yeSI6IiIsInByb2R1Y3RfYmFyY29kZSI6IjAiLCJwcm9kdWN0X3NrdSI6IiIsInByb2R1Y3RfZGlzY291bnQiOiIiLCJwcm9kdWN0X2ltYWdlIjoiaHR0cHM6XC9cL2ltYWdlcy5hZGFkYXB0ZWQuY29tXC8ifV19"
-//        private const val TEST_FAIL_URL_STRING = "droidrecipe://adadapted.com/addit_add_list_items?data=UNKNOWN"
-//        private const val TEST_UNKNOWN_URL_STRING = "droidrecipe://adadapted.com/unknown"
-//    }
-//}
+@OptIn(ExperimentalCoroutinesApi::class)
+@RunWith(RobolectricTestRunner::class)
+class DeeplinkContentParserTest {
+    private var testTransporter = UnconfinedTestDispatcher()
+    private val testTransporterScope: TransporterCoroutineScope = TestTransporter(testTransporter)
+
+    @Before
+    fun setup() {
+        DeviceInfoClient.createInstance("", false, HashMap(), "", TestDeviceInfoExtractor(), testTransporterScope)
+        SessionClient.createInstance(mock(), mock())
+        EventClient.createInstance(TestEventAdapter, testTransporterScope)
+        PayloadClient.createInstance(mock(), EventClient, mock())
+    }
+
+    @Test
+    fun parse() {
+        val uri = Uri.parse(TEST_URL_STRING_ITEMS)
+        val parser = DeeplinkContentParser()
+        val content = parser.parse(uri)
+        Assert.assertEquals("7498E63F-BF44-466B-A391-8B6721C32FF7", content.payloadId)
+        Assert.assertEquals("Sample Product", content.message)
+        Assert.assertEquals("", content.image)
+        Assert.assertEquals(AdditContent.AdditSources.DEEPLINK, content.additSource)
+        Assert.assertEquals(AddToListContent.Sources.OUT_OF_APP, content.getSource())
+        Assert.assertEquals(1, content.getItems().size.toLong())
+    }
+
+    @Test
+    fun fail() {
+        val parser = DeeplinkContentParser()
+
+        try {
+            parser.parse(null)
+            Assert.assertEquals(EventStrings.ADDIT_NO_DEEPLINK_RECEIVED, TestEventAdapter.testSdkErrors.first().code)
+            Assert.fail()
+        } catch (ex: Exception) {
+            //success
+        }
+    }
+
+    @Test
+    fun failWithParseFailure() {
+        try {
+            val uri = Uri.parse(TEST_FAIL_URL_STRING)
+            val parser = DeeplinkContentParser()
+            parser.parse(uri)
+            Assert.assertEquals(EventStrings.ADDIT_PAYLOAD_PARSE_FAILED, TestEventAdapter.testSdkErrors.first().code)
+        } catch (ex: Exception) {
+            //success
+        }
+    }
+
+    @Test
+    fun failWithPayloadFailure() {
+        try {
+            val uri = Uri.parse(TEST_UNKNOWN_URL_STRING)
+            val parser = DeeplinkContentParser()
+            parser.parse(uri)
+            Assert.assertEquals(EventStrings.ADDIT_PAYLOAD_PARSE_FAILED, TestEventAdapter.testSdkErrors.first().code)
+        } catch (ex: Exception) {
+            //success
+        }
+    }
+
+    companion object {
+        private const val TEST_URL_STRING_ITEMS = "droidrecipe://adadapted.com/addit_add_list_items?data=eyJwYXlsb2FkX2lkIjoiNzQ5OEU2M0YtQkY0NC00NjZCLUEzOTEtOEI2NzIxQzMyRkY3IiwicGF5bG9hZF9tZXNzYWdlIjoiU2FtcGxlIFByb2R1Y3QiLCJwYXlsb2FkX2ltYWdlIjoiIiwiY2FtcGFpZ25faWQiOiIyNTQiLCJhcHBfaWQiOiJkcm9pZHJlY2lwZSIsImV4cGlyZV9zZWNvbmRzIjo2MDQ4MDAsImRldGFpbGVkX2xpc3RfaXRlbXMiOlt7InRyYWNraW5nX2lkIjoiRDA2OTk4OTItQkY5RS00RTM1LUI5MkQtQzVEN0YyRDZFOUFEIiwicHJvZHVjdF90aXRsZSI6IlNhbXBsZSBQcm9kdWN0IiwicHJvZHVjdF9icmFuZCI6IkJyYW5kIiwicHJvZHVjdF9jYXRlZ29yeSI6IiIsInByb2R1Y3RfYmFyY29kZSI6IjAiLCJwcm9kdWN0X3NrdSI6IiIsInByb2R1Y3RfZGlzY291bnQiOiIiLCJwcm9kdWN0X2ltYWdlIjoiaHR0cHM6XC9cL2ltYWdlcy5hZGFkYXB0ZWQuY29tXC8ifV19"
+        private const val TEST_FAIL_URL_STRING = "droidrecipe://adadapted.com/addit_add_list_items?data=UNKNOWN"
+        private const val TEST_UNKNOWN_URL_STRING = "droidrecipe://adadapted.com/unknown"
+    }
+}
