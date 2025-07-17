673219622a193e6cc988b7ffd61380ea1385539d
Brett Clifton <bclifton@adadapted.com>
2025-07-17 11:21:45 -0400

    feat: gradle and dependencies updates

--- app/src/main/java/com/adadapted/sdktestapp/ui/SingleFragmentActivity.java
+++ app/src/main/java/com/adadapted/sdktestapp/ui/SingleFragmentActivity.java
@@ -1,8 +1,13 @@
 package com.adadapted.sdktestapp.ui;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import com.adadapted.sdktestapp.R;
@@ -13,8 +18,14 @@ public abstract class SingleFragmentActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
         setContentView(R.layout.activity_single_fragment);
-
+        View rootView = findViewById(R.id.content_frame);
+        ViewCompat.setOnApplyWindowInsetsListener(rootView, (view, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            view.setPadding(0, 140, 0, systemBars.bottom);
+            return insets;
+        });
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
                 .replace(R.id.content_frame, createFragment())
