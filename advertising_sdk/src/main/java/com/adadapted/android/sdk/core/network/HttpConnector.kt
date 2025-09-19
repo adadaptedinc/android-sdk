a5fa371f77448d827ecd34a7f79573e65dbb9ed6
Brett Clifton <bclifton@adadapted.com>
2025-09-19 10:48:23 -0400

    feat: updates for dependencies

--- advertising_sdk/src/main/java/com/adadapted/android/sdk/core/network/HttpConnector.kt
+++ advertising_sdk/src/main/java/com/adadapted/android/sdk/core/network/HttpConnector.kt
@@ -1,7 +1,6 @@
 package com.adadapted.android.sdk.core.network
 
 import com.adadapted.android.sdk.core.log.AALogger
-import io.ktor.client.engine.*
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
 import io.ktor.client.plugins.*
@@ -10,14 +9,12 @@ import io.ktor.client.plugins.observer.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.Json
 
-val defaultPlatformEngine: HttpClientEngine = Android.create()
-
 object HttpConnector {
     const val API_HEADER = "X-API-KEY"
     const val ENCODING_HEADER = "Accept-Encoding"
     const val ENCODING_FORMATS = "gzip, deflate"
 
-    val client = HttpClient {
+    val client = HttpClient(Android.create()) {
         install(ContentNegotiation) {
             json(Json {
                 useAlternativeNames = false
--- advertising_sdk/src/main/java/com/adadapted/android/sdk/core/network/HttpSessionAdapter.kt
+++ advertising_sdk/src/main/java/com/adadapted/android/sdk/core/network/HttpSessionAdapter.kt
@@ -14,6 +14,7 @@ import com.adadapted.android.sdk.core.view.ZoneContext
 import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
 
@@ -35,8 +36,11 @@ class HttpSessionAdapter(
             }
 
             //REMOVE
+            val bat = response.bodyAsText()
+            AALogger.logInfo(bat)
             val encoding = response.headers["Content-Encoding"] ?: "none"
             AALogger.logInfo("sendInit response Content-Encoding: $encoding")
+
 
             listener.onSessionInitialized(response.body<Session>().apply { this.deviceInfo = deviceInfo })
 
